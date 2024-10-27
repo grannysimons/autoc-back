a2e7eb2fcc3426d1a9c8b54ffc55428e4b37aaab
Mariona Roca <marionaroca@MacBook-MacBook-Pro-de-Mariona.local>
2024-10-28 00:05:09 +0100

    cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors')
 
 var corsOptions = {
-    origin: 'https://autocuidado.netlify.app/',
+    origin: '*',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 app.use(cors(corsOptions))
